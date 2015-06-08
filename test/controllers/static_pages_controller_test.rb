845b4722095eae426450e0d8728699f9f0a81990
Kokubo Atsushi <kokubo@aomori-u.ac.jp>
2015-06-08 19:14:25 +0900

    静的ページの作成の終了

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,10 +4,12 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "ホーム | 自然観察記録アプリ(練習用)"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "アプリについて | 自然観察記録アプリ(練習用)"
   end
 end
