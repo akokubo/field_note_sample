85e60de43d108f2b65f7a8bc3eb4c0797bf33e17
Kokubo Atsushi <kokubo@aomori-u.ac.jp>
2015-06-08 19:25:57 +0900

    重複の解消(ex.3.1)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,15 +1,19 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+  	@base_title = "自然観察記録アプリ(練習用)"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "ホーム | 自然観察記録アプリ(練習用)"
+    assert_select "title", "ホーム | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "アプリについて | 自然観察記録アプリ(練習用)"
+    assert_select "title", "アプリについて | #{@base_title}"
   end
 end
