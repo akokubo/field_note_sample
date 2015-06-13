e56d5b8faa4c650dedb9fa31775b37f64d54cca9
Kokubo Atsushi <kokubo@aomori-u.ac.jp>
2015-06-13 15:39:17 +0900

    ログイン/ログアウトの実装の完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "自然観察記録アプリへようこそ！"
       redirect_to @user
     else
